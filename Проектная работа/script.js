a3409ee8aeb67d92772ae695e3af66fda7485556
ChiriNNi <103880918+ChiriNNi@users.noreply.github.com>
2023-10-17 21:43:28 +0600

    Реализация функции проекта
    - Поиск по ключевому слову
    - Фильтрация по данным
    - Переходы между страницами
    - Оптимизация скрипта (функции и т.д)
    - Настройка отправки запросов

--- Проектная работа/script.js
+++ Проектная работа/script.js
@@ -1,9 +1,49 @@
+const apiKey = "68aaf7ab-50f7-4ff2-a18f-44511ece9a28";
+const movieContainer = document.getElementById("movieContainer");
+let currentFunction; 
+let currentArgument;
+
+function createFilmContainer(movies) {
+  movies.forEach((movie) => {
+    // Главный контейнер
+    const movieDiv = document.createElement("div");
+    movieDiv.classList.add("grid-item");
+
+    // Контейнер для изображения
+    const movieImageDiv = document.createElement("div")
+    
+    // Контейнер для информации о фильме
+    const movieInfoDiv = document.createElement("div")
+    movieInfoDiv.classList.add("grid-cont")
+
+    // Название фильма
+    const title = document.createElement("h1");
+    title.classList.add("grid-title")
+    title.textContent = movie.nameRu;
+
+    const description = document.createElement("p");
+    description.textContent = movie.description;
+
+    const image = document.createElement("img");
+    image.src = movie.posterUrl;
+    image.alt = movie.name;
+    image.classList.add("grid-image")
+
+    movieImageDiv.appendChild(image)
+    movieDiv.appendChild(movieImageDiv)
+
+    movieInfoDiv.appendChild(title)
+    movieInfoDiv.appendChild(description)
+    movieDiv.appendChild(movieInfoDiv)
+
+    movieContainer.appendChild(movieDiv);
+  });
+}
+
+// Загрузка контента при запуске сайта
 document.addEventListener("DOMContentLoaded", function () {
-    const apiKey = "68aaf7ab-50f7-4ff2-a18f-44511ece9a28";
-    const movieContainer = document.getElementById("movieContainer");
-  
     const xhr = new XMLHttpRequest();
-    xhr.open("GET", "https://kinopoiskapiunofficial.tech/api/v2.1/films/top?type=TOP_100_POPULAR_FILMS&page=1", true);
+    xhr.open("GET", `https://kinopoiskapiunofficial.tech/api/v2.1/films/top?type=TOP_100_POPULAR_FILMS&page=${currentPage}`, true);
     xhr.setRequestHeader("X-API-KEY", apiKey);
   
     xhr.onreadystatechange = function () {
@@ -12,100 +52,94 @@ document.addEventListener("DOMContentLoaded", function () {
         const data = JSON.parse(response);
         const movies = data.films;
   
-        movies.forEach((movie) => {
-          // Главный контейнер
-          const movieDiv = document.createElement("div");
-          movieDiv.classList.add("grid-item");
-
-          // Контейнер для изображения
-          const movieImageDiv = document.createElement("div")
-          
-          // Контейнер для информации о фильме
-          const movieInfoDiv = document.createElement("div")
-          movieInfoDiv.classList.add("grid-cont")
-
-          // Название фильма
-          const title = document.createElement("h1");
-          title.classList.add("grid-title")
-          title.textContent = movie.nameRu;
-
-          const description = document.createElement("p");
-          description.textContent = movie.description;
-  
-          const image = document.createElement("img");
-          image.src = movie.posterUrl;
-          image.alt = movie.name;
-          image.classList.add("grid-image")
-
-          movieImageDiv.appendChild(image)
-          movieDiv.appendChild(movieImageDiv)
-
-          movieInfoDiv.appendChild(title)
-          movieInfoDiv.appendChild(description)
-          movieDiv.appendChild(movieInfoDiv)
-
-          movieContainer.appendChild(movieDiv);
-        });
+        createFilmContainer(movies)
       }
     };
-  
+
     xhr.send();
-  });
+});
 
 
-  document.addEventListener("DOMContentLoaded", function () {
-    const apiKey = "68aaf7ab-50f7-4ff2-a18f-44511ece9a28";
-    const movieContainer = document.getElementById("movieContainer");
-    const genreRadios = document.querySelectorAll("input[name='genre']");
-    const countryRadios = document.querySelectorAll("input[name='country']");
-    const ratingRadios = document.querySelectorAll("input[name='rating']");
+// Фильтрация по данным
+function fetchMoviesByGenre(array) {
+  currentFunction = fetchMoviesByGenre
 
-    // Функция для отправки запроса на сервер с выбранными фильтрами
-    function fetchMoviesWithFilters() {
-        const selectedGenre = document.querySelector("input[name='genre']:checked").value;
-        const selectedCountry = document.querySelector("input[name='country']:checked").value;
-        const selectedRating = document.querySelector("input[name='rating']:checked").value;
+  const xhr = new XMLHttpRequest();
+  xhr.open("GET", `https://kinopoiskapiunofficial.tech/api/v2.2/films?genres=${array[0]}&page=${currentPage}&country=${array[1]}`, true);
+  xhr.setRequestHeader("X-API-KEY", apiKey);
 
-        const url = `https://kinopoiskapiunofficial.tech/api/v2.1/films/top?type=TOP_100_POPULAR_FILMS&page=1&genre=${selectedGenre}&country=${selectedCountry}&rating=${selectedRating}`;
+  xhr.onreadystatechange = function () {
+      if (xhr.readyState === 4 && xhr.status === 200) {
+          const response = xhr.responseText;
+          const data = JSON.parse(response);
+          const movies = data.items;
 
-        const xhr = new XMLHttpRequest();
-        xhr.open("GET", url, true);
-        xhr.setRequestHeader("X-API-KEY", apiKey);
+          movieContainer.innerHTML = "";
 
-        xhr.onreadystatechange = function () {
-            if (xhr.readyState === 4 && xhr.status === 200) {
-                const response = xhr.responseText;
-                const data = JSON.parse(response);
-                const movies = data.films;
+          createFilmContainer(movies)
+      }
+  };
+  xhr.send();
+}
 
-                // Очистка контейнера перед добавлением новых фильмов
-                movieContainer.innerHTML = "";
+// Обработчик нажатия кнопки "Применить фильтр"
+document.getElementById("applyFilterButton").addEventListener("click", function () {
+  const selectedGenre = document.querySelector('input[name="genre"]:checked').value;
+  const selectedCountry = document.querySelector('input[name="country"]:checked').value;
+  currentArgument = [selectedGenre, selectedCountry]; 
+  fetchMoviesByGenre([selectedGenre, selectedCountry]);
+});
 
-                movies.forEach((movie) => {
-                    // Создание элементов фильма как ранее
-                    // ...
 
-                    movieContainer.appendChild(movieDiv);
-                });
-            }
-        };
+// Поиск по ключевому слову
+const movieSearchInput = document.getElementById("movieSearchInput");
+const movieSearchButton = document.getElementById("movieSearchButton");
 
-        xhr.send();
-    }
+function searchMovies(query) {
+  currentFunction = searchMovies
 
-    // Обработчики событий для радио кнопок (и других элементов фильтрации)
-    genreRadios.forEach((radio) => {
-        radio.addEventListener("change", fetchMoviesWithFilters);
-    });
+  const xhr = new XMLHttpRequest();
+  xhr.open("GET", `https://kinopoiskapiunofficial.tech/api/v2.2/films?keyword=${query}&page=${currentPage}`, true);
+  xhr.setRequestHeader("X-API-KEY", apiKey);
 
-    countryRadios.forEach((radio) => {
-        radio.addEventListener("change", fetchMoviesWithFilters);
-    });
+  xhr.onreadystatechange = function () {
+      if (xhr.readyState === 4 && xhr.status === 200) {
+          const response = xhr.responseText;
+          const data = JSON.parse(response);
+          const movies = data.items;
 
-    ratingRadios.forEach((radio) => {
-        radio.addEventListener("change", fetchMoviesWithFilters);
-    });
+          movieContainer.innerHTML = ""; 
 
-    // Инициализация загрузки фильмов при загрузке страницы
-    fetchMoviesWithFilters();
+          createFilmContainer(movies)
+      }
+  };
+
+  xhr.send();
+}
+
+movieSearchButton.addEventListener("click", function () {
+    const searchQuery = movieSearchInput.value;
+    currentArgument = searchQuery
+    searchMovies(searchQuery);
+});
+
+
+// Перемещение по страницам
+let currentPage = 1; 
+const currentPageSpan = document.getElementById("currentPage");
+const nextPageButton = document.getElementById("nextPageButton");
+const prevPageButton = document.getElementById("prevPageButton");
+
+nextPageButton.addEventListener("click", function () {
+  currentPage++;
+  currentFunction(currentArgument);
+  currentPageSpan.textContent = currentPage;
+});
+
+prevPageButton.addEventListener("click", function () {
+  if (currentPage > 1) {
+      currentPage--;
+      currentFunction(currentArgument);
+      currentPageSpan.textContent = currentPage;
+  }
 });
